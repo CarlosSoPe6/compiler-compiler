5fda54342fcc1937951385ad91c9b0f2f02e482a
Carlos Soto Pérez <carlos348@outlook.com>
2021-05-02 16:44:36 -0500

    CUP Automaton error. Regla CP y terminales KEYWORD_TERMINAL y KEYWORD_NONTERM añadidos.

---
+++ src/mx/jcc/syntax/AssigmentNode.java
@@ -0,0 +1,11 @@
+package mx.jcc.syntax;
+
+public class AssigmentNode {
+    public String a;
+    public String h;
+
+    public AssigmentNode(String a, String h) {
+        this.a = a;
+        this.h = h;
+    }
+}
